495b76ca98e03a7ba1d88aca7150d30c20e34662
Alejandro Minor <AlejandroMinor@outlook.com>
2026-09-03 21:52:46 -0600

    style: make kitty more transparent and invert fastfetch skull

--- dotconfig/hypr/windowrules.lua
+++ dotconfig/hypr/windowrules.lua
@@ -119,7 +119,7 @@ hl.window_rule({
 hl.window_rule({
     name    = "kitty-transparent",
     match   = { class = "^(kitty)$" },
-    opacity = "0.85 override 0.85 override",
+    opacity = "0.70 override 0.70 override",
 })
 
 hl.window_rule({
